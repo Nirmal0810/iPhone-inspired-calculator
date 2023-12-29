9f112a16f7c860a17a756f1d6143cac93d0d559d
Nirmal <gnirmal0810@gmail.com>
2023-12-29 16:49:16 +0530

    First Commit to calculator app

---
+++ index.js
@@ -0,0 +1,19 @@
+const display = document.getElementById("display")
+console.log(display)
+
+function pushToDisplay(input){
+    display.value+=input
+}
+
+function clearDisplay(){
+    display.value=""
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value)
+    }
+    catch{
+        display.value = "Error"
+    }
+}
